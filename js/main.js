9d813dd0ded0a81afea3001c657a37b62d3e17ca
Amitosh Anand <amitex007@gmail.com>
2020-11-22 21:43:34 +0530

    loaders added

--- js/main.js
+++ js/main.js
@@ -20,3 +20,29 @@ window.addEventListener('load', ()=>{
     let preloader = document.getElementById('preloader');
     preloader.classList.add('post-finish');
 })
+
+$(document).ready(function(){
+    $("#successRequest").hide();
+    $("#failureRequest").hide();
+    $("#cvloader").hide();
+
+    $("#resumeForm").submit(function(){
+        console.log("Form post received")
+        $("#resumeForm").hide();
+        $("#cvMessage").hide();
+        $("#cvloader").show();
+        $.post($(this).attr('action'), $(this).serialize(), function(response){
+            // do something here on success
+            console.log(response);
+
+            $("#cvloader").hide();
+
+            if(response.result=="success") {
+                $("#successRequest").show();
+            } else {
+                $("#failureRequest").hide();
+            }
+      },'json');
+      return false;
+    })
+});
